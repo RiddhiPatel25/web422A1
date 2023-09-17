ec47a308e7bd5d476d7f67da5764471b41019081
RiddhiPatel25 <122702771+RiddhiPatel25@users.noreply.github.com>
2023-09-17 19:42:47 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,102 @@
+/*********************************************************************************
+ *
+ * WEB422 – Assignment 1
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Name: Riddhi Bharatkumar Patel
+ * Student ID: 151212214
+ * Date: September 17th 2023
+ *
+ * Cyclic Link:
+ *
+ ***********************************************************************************/
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ msg: "API Listening" });
+});
+
+const cors = require("cors");
+app.use(cors());
+
+require("dotenv").config();
+
+app.use(express.json());
+
+const MoviesDB = require("./modules/moviesDB.js");
+const db = new MoviesDB();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/api/movies", (req, res) => {
+  db.addNewMovie(req.body)
+    .then((data) => {
+      res.status(201).json(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
+app.get("/api/movies", (req, res) => {
+  db.getAllMovies(req.query.page, req.query.perPage, req.query.title)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(404).json({ message: err });
+    });
+});
+
+app.get("/api/movies/:id", (req, res) => {
+  const id = req.params.id;
+  db.getMovieById(id)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      const msg = `Cannot find Movie id: ${id} in the database. ERROR: ${err}`;
+      console.log(msg);
+      res.status(204).json(msg);
+    });
+});
+
+app.put("/api/movies/:id", (req, res) => {
+  const id = req.params.id;
+  db.updateMovieById(req.body, id)
+    .then(() => {
+      res.send(`${req.body.title} updated.`);
+    })
+    .catch((err) => {
+      res.status(500).send(`Cannot update Movie. ERROR: ${err}`);
+    });
+});
+
+app.delete("/api/movies/:id", (req, res) => {
+  const id = req.params.id;
+  db.deleteMovieById(id)
+    .then(() => {
+      res.send(`Movie id: ${id} deleted`);
+    })
+    .catch((err) => {
+      res
+        .status(204)
+        .send(`Cannot find Movie id: ${id} in the database. ERROR: ${err}`);
+    });
+});
+
+db.initialize(process.env.MONGODB_CONN_STRING)
+  .then(() => {
+    app.listen(HTTP_PORT, () => {
+      console.log(`server listening on: ${HTTP_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
